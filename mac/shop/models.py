3f969e38e58588a4b6e76e32c798ea7568e38b43
SyedHassanUlHaq <21b-029-cs@students.uit.edu>
2023-10-13 01:44:28 +0500

    implemented backend for checkout feature

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
-    image = models.ImageField(upload_to="shop/images", default="")
+    image = models.ImageField(upload_to='shop/images', default="")
 
     def __str__(self):
         return self.product_name
@@ -22,5 +22,26 @@ class Contact(models.Model):
     phone = models.CharField(max_length=70, default="")
     desc = models.CharField(max_length=500, default="")
 
+
     def __str__(self):
         return self.name
+
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=5000)
+    name = models.CharField(max_length=90)
+    email = models.CharField(max_length=111)
+    address = models.CharField(max_length=111)
+    city = models.CharField(max_length=111)
+    state = models.CharField(max_length=111)
+    zip_code = models.CharField(max_length=111)
+    phone = models.CharField(max_length=111, default="")
+
+class OrderUpdate(models.Model):
+    update_id  = models.AutoField(primary_key=True)
+    order_id = models.IntegerField(default="")
+    update_desc = models.CharField(max_length=5000)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.update_desc[0:7] + "..."
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
-from .models import Product, Contact
+from .models import Product, Contact, Orders, OrderUpdate
 from math import ceil
-# import the logging library
-import logging
+import json
 
-# Get an instance of a logger
-logger = logging.getLogger(__name__)
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
-    # products = Product.objects.all()
-    # print(products)
-    # n = len(products)
-    # nSlides = n//4 + ceil((n/4)-(n//4))
     allProds = []
     catprods = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catprods}
@@ -22,15 +16,13 @@ def index(request):
         n = len(prod)
         nSlides = n // 4 + ceil((n / 4) - (n // 4))
         allProds.append([prod, range(1, nSlides), nSlides])
-
-    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
-    # allProds = [[products, range(1, nSlides), nSlides],
-    #             [products, range(1, nSlides), nSlides]]
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
+
 def about(request):
     return render(request, 'shop/about.html')
+
 
 def contact(request):
     if request.method=="POST":
@@ -42,18 +34,55 @@ def contact(request):
         contact.save()
     return render(request, 'shop/contact.html')
 
+
 def tracker(request):
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps([updates, order[0].items_json], default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+
     return render(request, 'shop/tracker.html')
+
 
 def search(request):
     return render(request, 'shop/search.html')
 
+
 def productView(request, myid):
+
     # Fetch the product using the id
     product = Product.objects.filter(id=myid)
-
-
     return render(request, 'shop/prodView.html', {'product':product[0]})
 
+
 def checkout(request):
+    if request.method=="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
+        update.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html', {'thank':thank, 'id': id})
     return render(request, 'shop/checkout.html')
